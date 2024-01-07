2bd442595ffc58a55ea54467ad998144c3988f6a
zafar4ek <zafarsoliyev11@gmail.com>
2024-01-08 02:09:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,173 @@
+/// For-1 ///////////////////////////////////
+// let a = 1;
+// let b = 10; 
+// if ( a < b ) {
+//     for (let i = a; i <= b; i++) {
+//         console.log(i);
+        
+//     }
+//     let number = `Chiqarilgan sonlar soni ${b} ta` ;
+//     console.log(number);
+// }
+
+
+// ////// For-2 //////////////////////////
+// let a = 1;
+// let b = 100;
+// if ( a < b ) {
+//     for (let i = b - 1; i >= a + 1; i--) {
+//     console.log(i); 
+//     }
+
+//     console.log(`Chiqarilgan sonlar soni ${b-2} ta`);
+           
+// }
+
+
+/////// For-3 //////////////////////////
+// let kg = 10000; 
+// for (let i = 1; i <= 2; ) {
+//     console.log(i);
+    
+// }
+
+
+/////// For - 5 ///////////////////////
+// let a = 1;
+// let b = 10; 
+// let str = 0; 
+// let result = 0;
+// if ( a < b ) {
+//     for (let i = a; i <= b; i++) {
+//         str = i; 
+//         result = str + i; 
+//         console.log(result);
+//     }
+// }
+
+
+/////// For - 6 //////////////////////
+// let a = 1;
+// let b = 10; 
+// let str = 0; 
+// let result = 0;
+// if ( a < b ) {
+//     for (let i = a; i <= b; i++) {
+//         str = i; 
+//         result = str * i; 
+//         console.log(result);
+//     }
+// }
+
+/////// For - 7 ///////////////////
+// let a = 1;
+// let b = 10; 
+// let str = 0; 
+// let srr = 0; 
+// let stt = 0; 
+// if ( a < b ) {
+//     for (let i = a; i <= b; i++) {
+//         str = i; 
+//         srr = str * i; 
+//         stt += srr;  
+//         console.log(srr);
+//     }
+//     console.log(`Kvadratning Yigindisi: ${stt}`);
+// }
+
+
+
+////////// For - 8 //////////////
+// let a = 1;
+// let n = 10;
+// let x = 0;  
+// if (n > 0) {
+//     for (let i = a; i <= n; i++) {
+//         x = i += 1; 
+//         console.log(x);
+//     }
+
+// }
+
+// let a = 1;
+// let n = 10;
+// let x = 0;  
+// if (n > 0) {
+//     for (let i = a; i <= n; i++) {
+//         x = i *= 1; 
+//         console.log(x); 
+//     }
+
+// }
+
+
+
+/////////// For-11 ////////////////////////////////////
+// let n = 5
+// let s = 0
+// for (let i = 1; i<=n;i++){
+//     s = s + (2*i -1)
+//    console.log(s)
+// }
+
+////////// For-12 ///////////////////////////
+// let a = 5;                                                          
+// let n = 5;                                                                   
+// let x = 0 ; 
+// if (n > 0) {
+//     for (let i = 1; i <=n ; i++) {
+//         x = a ** i; 
+//         console.log(x);
+//     }
+//     console.log(`Sonning kvadrati ${x};`);
+// }
+
+//////////// For-13 //////////////////////////
+// let a = 5; 
+// let n = 5; 
+// let x = 0 ;                                                  Bu formula 12 va 13 masalalarga hizmat qila oladi menmcha shu sababli ikkalasiga ham ishlatilgan.
+// if (n > 0) {
+//     for (let i = 1; i <=n ; i++) {
+//         x = a ** i; 
+//         console.log(x);
+//     }
+//     console.log(`Sonning kvadrati ${x};`);
+// }
+
+///////////////////// For-14 /////////////////////////////////////
+// let a = 5; 
+// let n = 5; 
+// let x = 0 ;      
+// let y = 0;                                            
+// if (n > 0) {
+//     for (let i = 1; i <=n ; i++) {
+//         x = a ** i; 
+//         y += x;
+//         console.log(y);
+//     }
+//     console.log(`Sonning kvadrati ${x};`);
+//     console.log(`Sonning barcha darajalar yigindisi ${y} ga teng`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
